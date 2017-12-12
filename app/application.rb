f216b6af8d89a5f8e169044ea9621be0804b7919
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-12 16:31:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,6 +15,9 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
       resp.write @@cart.join("\n")
     elsif req.path.match(/add/)
       cart_item = req.params["c"]
